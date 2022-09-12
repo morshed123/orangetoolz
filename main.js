5d5b9c906e07acddab315a47d5ce68d697140b55
md-morshed-hossain <mdmorshedhossain38@gmail.com>
2022-09-13 00:18:26 +0600

    added to all 12-sep-2022

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ sub_links.forEach((link) => {
 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+function DropdownFunc() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
